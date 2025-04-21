1e4b8962074c58b7a54c986d0ffa7bee50df3624
teddyld <vincent.pham2@outlook.com>
2025-04-22 00:45:46 +1000

    Added vendor files and used glm to project texture to correct resolution

--- OpenGL/OpenGL/src/Shader.h
+++ OpenGL/OpenGL/src/Shader.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "glm/glm.hpp"
 
 #include <string>
 #include <unordered_map>
@@ -24,6 +25,7 @@ public:
 
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
 	unsigned int GetUniformLocation(const std::string& name);
 	ShaderProgramSource ParseShader(const std::string& filepath);
