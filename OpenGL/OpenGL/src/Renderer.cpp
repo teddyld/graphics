44217fc1f4397978f08bc0bde4dcf6a3c04083b5
teddyld <vincent.pham2@outlook.com>
2025-04-29 15:12:30 +1000

    Removed EBO binding in rendering stage because VAO automatically binds EBO

--- OpenGL/OpenGL/src/Renderer.cpp
+++ OpenGL/OpenGL/src/Renderer.cpp
@@ -28,7 +28,6 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
 {
 	shader.Bind();
 	va.Bind();
-	ib.Bind();
 
 	glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
 }
